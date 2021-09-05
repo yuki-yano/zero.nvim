81d94fc4555e109e78785bd406106f806c781402
yuki_yano <yuki.ycino@gmail.com>
2021-09-05 19:44:48 +0900

    Add xnoremap

--- lua/zero.lua
+++ lua/zero.lua
@@ -1,5 +1,6 @@
 local function setup()
   vim.api.nvim_set_keymap('n', '0', "getline('.')[0 : col('.') - 2] =~# '^\\s\\+$' ? '0' : '^'", {silent = true, noremap = true, expr = true})
+  vim.api.nvim_set_keymap('x', '0', "getline('.')[0 : col('.') - 2] =~# '^\\s\\+$' ? '0' : '^'", {silent = true, noremap = true, expr = true})
 end
 
 return {setup = setup}
