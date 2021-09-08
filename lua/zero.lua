ec0b1adcb3a34fad96c7a04bf0130db716f66e8c
yuki_yano <yuki.ycino@gmail.com>
2021-09-08 12:43:56 +0900

    Add onoremap

--- lua/zero.lua
+++ lua/zero.lua
@@ -1,6 +1,7 @@
 local function setup()
   vim.api.nvim_set_keymap('n', '0', "getline('.')[0 : col('.') - 2] =~# '^\\s\\+$' ? '0' : '^'", {silent = true, noremap = true, expr = true})
   vim.api.nvim_set_keymap('x', '0', "getline('.')[0 : col('.') - 2] =~# '^\\s\\+$' ? '0' : '^'", {silent = true, noremap = true, expr = true})
+  vim.api.nvim_set_keymap('o', '0', "getline('.')[0 : col('.') - 2] =~# '^\\s\\+$' ? '0' : '^'", {silent = true, noremap = true, expr = true})
 end
 
 return {setup = setup}
